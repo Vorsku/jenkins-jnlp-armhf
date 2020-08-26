f25cbb3233077801995fd7c0f76cc7552dbf937f
Vorsku <vorsku@gmail.com>
2020-08-26 09:51:53 +0100

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+JENKINS_VERSION=4.3
+PREFIX="vorsku"
+REPO="jenkins-jnlp-armhf"
+
+docker build \
+	--rm --tag $PREFIX/$REPO:$JENKINS_VERSION \
+	--no-cache --file Dockerfile .
+	
+docker tag $PREFIX/$REPO:$JENKINS_VERSION $PREFIX/$REPO:latest
+docker push $PREFIX/$REPO:$JENKINS_VERSION
+docker push $PREFIX/$REPO:latest
