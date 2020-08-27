5612a0b13d661546a0c77a84ebc3b232b19217e7
Vorsku <vorsku@gmail.com>
2020-08-27 09:52:41 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
-IMAGE_VERSION=dev
+IMAGE_VERSION=0.2
 PREFIX="vorsku"
 REPO="jenkins-ssh-agent-armhf"
 
 docker build \
+	--rm --tag $PREFIX/$REPO:$JENKINS_VERSION \
 	--rm --tag $PREFIX/$REPO:$IMAGE_VERSION \
 	--no-cache --file Dockerfile .
-	
-docker tag $PREFIX/$REPO:$IMAGE_VERSION
+
+docker tag $PREFIX/$REPO:$JENKINS_VERSION $PREFIX/$REPO:latest
+docker push $PREFIX/$REPO:$JENKINS_VERSION
+docker tag $PREFIX/$REPO:$IMAGE_VERSION $PREFIX/$REPO:latest
 docker push $PREFIX/$REPO:$IMAGE_VERSION
+docker push $PREFIX/$REPO:latest
